d5eb63f22624ebe67806fd485527059655582137
franciscovillaescusa <villaescusa.francisco@gmail.com>
2020-04-09 23:35:03 -0400

    code updated

--- symbolic_regression/symbolic_regression.py
+++ symbolic_regression/symbolic_regression.py
@@ -1,12 +1,17 @@
 import numpy as np
 from gplearn.genetic import SymbolicRegressor
 from gplearn.functions import make_function
+import sympy
+import sympy.printing as printing
 import sys,os
 
 # function used to generate the data
 def function(x):
     return 3*x**(3.5) + 2
 
+def _pow(x1,x2):
+    with np.errstate(over='ignore'):
+        return _protected_exponent(x2*_protected_log(x1))
 
 def _protected_exponent(x):
     with np.errstate(over='ignore'):
@@ -18,30 +23,31 @@ def _protected_log(x):
 
 exp = make_function(function=_protected_exponent, name='exp', arity=1)
 log = make_function(function=_protected_log,      name='log', arity=1)
+pow = make_function(function=_pow,                name='pow', arity=2)
 
 ################################### INPUT ###########################################
 points_training = 1000
 points_test     = 150
 
 # symbolic regressor parameters
-population_size       = 300
-generations           = 300
-tournament_size       = 10
-function_set          = ('add', 'sub', 'mul', 'div', exp, log)
+population_size       = 10000
+generations           = 30
+tournament_size       = 100
+function_set          = ('add', 'sub', 'mul', 'div', exp, log, pow)
 metric                = 'mse'
-init_depth            = (2, 15)
+init_depth            = (2, 8)
 n_jobs                = 1
 verbose               = 1
-parsimony_coefficient = 0.00001
+parsimony_coefficient = 0.0005
 random_state          = None
 
 # evolution parameters
 # p_crossover + p_subtree_mutation + p_hoist_mutation + p_point_mutation <= 1.0
-p_crossover        = 0.2
-p_subtree_mutation = 0.2
-p_hoist_mutation   = 0.2
-p_point_mutation   = 0.2
-p_point_replace    = 0.05
+p_crossover        = 0.3
+p_subtree_mutation = 0.25
+p_hoist_mutation   = 0.1
+p_point_mutation   = 0.25
+p_point_replace    = 0.3
 #####################################################################################
 
 # get training set
@@ -74,8 +80,30 @@ y_pred = model.predict(x_test)
 mse    = np.mean((y_test - y_pred)**2)
 print('mse = %.3e'%mse)
 
-print(model._program)
-print(y_pred.shape, y_test.shape)
-x_test = np.reshape(x_test, (-1,))
+# print best program
+program = model._program
+print(program)
 
+# save results to file
+x_test = np.reshape(x_test, (-1,))
 np.savetxt('results.txt', np.transpose([x_test, y_test, y_pred]))
+
+
+
+####### SYMPY #######
+converter = {
+    'sub': lambda x, y : x - y,
+    'div': lambda x, y : x/y,
+    'mul': lambda x, y : x*y,
+    'add': lambda x, y : x + y,
+    'neg': lambda x    : -x,
+    'pow': lambda x, y : x**y,
+}
+
+# simplify expression
+program_simp = sympy.sympify(program, locals=converter)
+
+# print simplified result
+print(program_simp)
+print('Latex formula:')
+print(sympy.latex(program_simp))
